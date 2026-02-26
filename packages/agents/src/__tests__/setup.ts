9f00e5ef6ede5a5832c3647104a21fcd156609aa
Jade-m22 <mj.micheljade@gmail.com>
2026-02-26 10:39:49 +0100

    chore: stop tracking tests

--- packages/agents/src/__tests__/setup.ts
+++
@@ -1,6 +0,0 @@
-import { config } from "dotenv";
-import { resolve } from "path";
-
-// Load env vars from root .env and apps/web/.env (which has GROQ keys)
-config({ path: resolve(__dirname, "../../../../.env") });
-config({ path: resolve(__dirname, "../../../../apps/web/.env") });
